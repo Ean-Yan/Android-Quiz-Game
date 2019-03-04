f035149e32dfe28f4512c1ac2f6dafe9729c2879
DDRMASTERM <ddrmasterm@gmail.com>
2019-03-04 10:43:40 -0700

    These are the starter questions and class that you should be able to use for testing.

---
+++ app/src/main/java/com/example/jacobgraves/myapplication/Database/Question.kt
@@ -0,0 +1,11 @@
+package com.example.jacobgraves.myapplication
+
+class Question(
+    var QType: String,
+    var QDiff: Int,
+    var Question: String,
+    var Answer1a: String,
+    var Answer2b: String,
+    var Answer3c: String,
+    var Answer4d: String,
+    var correct: Int)
