8c2b3511a44a8673f9b422d4a3775c9d4ba8e3c6
DDRMASTERM <ddrmasterm@gmail.com>
2019-03-25 10:16:58 -0600

    Updated gradle

--- app/src/main/java/com/example/jacobgraves/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/jacobgraves/myapplication/MainActivity.kt
@@ -8,5 +8,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val gameOn : Button = findViewById(R.id.PlayButton)
+        gameOn.setOnClickListener {
+            val toQuestionPage = Intent(this, gamePage :: class.java)
+            startActivity(toQuestionPage)
+        }
+
+        //Unsolved Media Switch
+        media = MediaPlayer.create(this, R.raw.homepage)
+        val music = findViewById<Button>(R.id.soundButton) as Button
+        media.start()
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
