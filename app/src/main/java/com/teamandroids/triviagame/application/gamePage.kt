1296b265c4863e826e38f0929ff8ab6c56b81698
DDRMASTERM <ddrmasterm@gmail.com>
2019-04-03 10:54:52 -0600

    Experimental changes to get randomized questions. Seems to crash from the onClickListener in Main page. Feel free to test if you can fix the error.

--- app/src/main/java/com/teamandroids/triviagame/application/gamePage.kt
+++ app/src/main/java/com/teamandroids/triviagame/application/gamePage.kt
@@ -3,6 +3,7 @@ package com.teamandroids.triviagame.application
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.teamandroids.triviagame.application.Database.QuestionBuilder
 import kotlinx.android.synthetic.main.activity_game_page.*
 
@@ -12,14 +13,18 @@ class gamePage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_page)
         var builder = QuestionBuilder().getQuestion("Science", 3)
+        /*var Question = findViewById<TextView>(R.id.QuestionView)
+        var Answer1a = findViewById<TextView>(R.id.Answer1a)
+        var Answer2b = findViewById<TextView>(R.id.Answer2b)
+        var Answer3c = findViewById<TextView>(R.id.Answer3c)
+        var Answer4d = findViewById<TextView>(R.id.Answer4d)
 
-        builder?.wasUsed=true
-        QuestionView.setText(builder?.Question)
+        builder?.wasUsed = true
+        Question.setText(builder?.Question)
         Answer1a.setText(builder?.Answer1a)
         Answer2b.setText(builder?.Answer2b)
         Answer3c.setText(builder?.Answer3c)
-        Answer4d.setText(builder?.Answer4d)
-
+        Answer4d.setText(builder?.Answer4d)*/
     }
 }
 
